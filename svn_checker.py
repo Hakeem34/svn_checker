1333300581b4c5abcf9181b0f028e95aba64da57
tkubota <tkubota@mx4.canvas.ne.jp>
2023-09-10 18:43:45 +0900

    add export function

--- svn_checker.py
+++ svn_checker.py
@@ -5,10 +5,12 @@ import datetime
 import subprocess
 import openpyxl
 import difflib
+import errno
 from pathlib import Path
 
 
 g_stop_on_copy = 0
+g_full_path    = 0
 g_log_limit    = 0
 g_kazoe_path   = ""
 g_revision1    = ""
@@ -17,9 +19,11 @@ g_path1        = ""
 g_path2        = ""
 g_path_logs    = []
 g_repo_info    = None
+g_out_path     = "out"
+g_target_paths = []
 
 re_log_line    = re.compile(r"^r([0-9]+)\s\|\s([^\|]+)\s\|\s([0-9]+)-([0-9]+)-([0-9]+)\s([0-9]+):([0-9]+):([0-9]+)\s[^\|]+\s\|\s([0-9]+)\sline")
-re_change_line = re.compile(r"^\s+([MAD])\s(.+)")
+re_change_line = re.compile(r"^\s+([MADR])\s(.+)")
 re_change_file = re.compile(r"\/([^\/]+)$")
 
 class cChangeFile:
@@ -51,6 +55,7 @@ class cPathLog:
         self.path     = ""
         self.option   = ""
         self.logs     = []
+        self.targets  = []
         return
 
 class cRepoInfo:
@@ -63,10 +68,42 @@ class cRepoInfo:
         return
 
 
+#/*****************************************************************************/
+#/* 外部コマンド実行                                                          */
+#/*****************************************************************************/
 def cmd_execute(cmd_text):
     result = subprocess.run(cmd_text, capture_output=True, text=True)
 #   print(result.stdout)
     return result.stdout
+
+
+#/*****************************************************************************/
+#/* ディレクトリ作成                                                          */
+#/*****************************************************************************/
+def make_directory(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    return
+
+
+#/*****************************************************************************/
+#/* ターゲットパス判定                                                        */
+#/*****************************************************************************/
+def is_path_in_target(path):
+    global g_target_paths
+
+    #/* ターゲットパス指定が空の場合は、trueを返す */
+    if not g_target_paths:
+        return True
+
+    for target in g_target_paths:
+        if (re.search(target, path)):
+            return True
+
+    return False
 
 
 #/*****************************************************************************/
@@ -80,6 +117,8 @@ def check_command_line_option():
     global g_revision2
     global g_path1
     global g_path2
+    global g_out_path
+    global g_full_path
 
     argc = len(sys.argv)
     option = ""
@@ -103,17 +142,24 @@ def check_command_line_option():
             g_log_limit = int(arg)
 #           print("log limit %s" % g_log_limit)
             option = ""
+        elif (option == "o"):
+            g_out_path = arg
+            option = ""
         elif (option == "k"):
             g_kazoe_path = arg
             option = ""
         elif (arg == "--stop-on-copy"):
             g_stop_on_copy = 1
+        elif (arg == "-f") or (arg == "--fullpath"):
+            g_full_path = 1
         elif (arg == "-l") or (arg == "--limit"):
             option = "l"
         elif (arg == "-r") or (arg == "--revision"):
             option = "r"
         elif (arg == "-k") or (arg == "--kazoe"):
             option = "k"
+        elif (arg == "-o"):
+            option = "o"
         elif (g_path1 == ""):
             if (result := re.search(r"\/$", arg)):
 #               print("end by /")
@@ -140,6 +186,7 @@ def check_command_line_option():
 #/*****************************************************************************/
 def check_log(target_path, revision, limit):
     global g_path_logs
+    global g_repo_info
 
     cmd_text = r"svn log -v "
     option   = ""
@@ -168,9 +215,11 @@ def check_log(target_path, revision, limit):
                 log_sequence = 1
                 if (commit_log != None):
                     path_log.logs.append(commit_log)
+                    commit_log = None
             elif (result := re_log_line.match(line)):
                 if (commit_log != None):
                     path_log.logs.append(commit_log)
+                    commit_log = None
 
                 commit_log = cCommitLog()
                 commit_log.revision = int(result.group(1))
@@ -182,7 +231,7 @@ def check_log(target_path, revision, limit):
                 commit_log.minute   = int(result.group(7))
                 commit_log.second   = int(result.group(8))
                 commit_log.line     = int(result.group(9))
-#               print("rev : " + result.group(1) + ", author : " + result.group(2) + ", line : " + result.group(9))
+                print("rev : " + result.group(1) + ", author : " + result.group(2) + ", line : " + result.group(9))
 #               print("datetime : %04d/%02d/%02d %02d:%02d:%02d" % (commit_log.year, commit_log.month, commit_log.day, commit_log.hour, commit_log.minute, commit_log.second))
                 log_sequence = 2
                 log_lines    = commit_log.line
@@ -207,8 +256,16 @@ def check_log(target_path, revision, limit):
                     changed.file_name = result.group(1)
                 else:
                     changed.file_name = ""
+                if (re.match(g_repo_info.relative, changed.path) ):
+                    #/* 指定されたリポジトリパスの範囲内のファイル */
+                    print("in type : %s, file : %s, path : %s" % (changed.attribute, changed.file_name, changed.path))
+                    changed.external = 0
+                    path_log.targets.append(changed.path)
+                else:
+                    #/* 指定されたリポジトリパスの範囲外のファイル */
+#                   print("ex type : %s, file : %s, path : %s" % (changed.attribute, changed.file_name, changed.path))
+                    changed.external = 1
                 commit_log.changes.append(changed)
-                print("type : %s, file : %s, path : %s" % (changed.attribute, changed.file_name, changed.path))
             else:
                 print("strange log in [%d] : %s" % (log_sequence, line))
         elif (log_sequence == 4):
@@ -218,7 +275,8 @@ def check_log(target_path, revision, limit):
                 log_sequence = 0
             pass
 
-
+    #/* 最後にターゲットpathの重複を削除して、登録する */
+    path_log.targets = set(path_log.targets)
     g_path_logs.append(path_log)
     return
 
@@ -273,6 +331,64 @@ def check_repo_info():
     return
 
 
+
+#/*****************************************************************************/
+#/* ログ内のターゲットファイル出力                                            */
+#/*****************************************************************************/
+def output_log_files(path_log, commit_log):
+    global g_repo_info
+    global g_full_path
+    global g_out_path
+
+    rev_path = g_out_path + "/rev_" + str(commit_log.revision)
+    cmd_base = "svn export -r " + str(commit_log.revision) + " "
+    make_directory(rev_path)
+
+    out_path = rev_path
+    for target in path_log.targets:
+        if (is_path_in_target(target)):
+#           print("out for %d : %s" % (commit_log.revision, target))
+            if (g_full_path):
+                out_path = rev_path + os.path.dirname(target)
+                export_cmd = cmd_base + g_repo_info.root + target + " " + out_path
+            else:
+                out_path = rev_path + os.path.dirname(target).replace(g_repo_info.relative, "")
+                export_cmd = cmd_base + g_repo_info.root + target + " " + out_path
+#           print("create path : %s" % (out_path))
+            make_directory(out_path)
+#           print("export : %s" % (export_cmd))
+            lines = cmd_execute(export_cmd)
+
+    return
+
+
+#/*****************************************************************************/
+#/* パス内のファイル履歴出力                                                  */
+#/*****************************************************************************/
+def output_path_files():
+    global g_repo_info
+    global g_path_logs
+    global g_out_path
+    global g_full_path
+
+    make_directory(g_out_path)
+
+    for path_log in g_path_logs:
+        for target in path_log.targets:
+            print("target : %s" % target)
+
+        for log in path_log.logs:
+#           print("log for rev : %d" % log.revision)
+            for changed in log.changes:
+                if (changed.external == 0):
+#                   print("hit on rev %d" % log.revision)
+                    if (is_path_in_target(changed.path)):
+                        output_log_files(path_log, log)
+                        break
+
+    return
+
+
 #/*****************************************************************************/
 #/* メイン関数                                                                */
 #/*****************************************************************************/
@@ -280,6 +396,7 @@ def main():
     check_command_line_option()
     check_repo_info()
     check_path_log()
+    output_path_files()
     return
 
 
